56fd501ddb46d053597663c375497ebcac7868ee
rato42 <lucasolt@gmail.com>
2024-12-14 23:13:12 -0300

    2.10a

--- Code/SOURCE_DynamicSpawnLandmine_GameInit.lua
+++ Code/SOURCE_DynamicSpawnLandmine_GameInit.lua
@@ -2,10 +2,12 @@ local original_DynamicSpawnLandmine_GameInit = DynamicSpawnLandmine.GameInit
 
 function DynamicSpawnLandmine:GameInit()
     original_DynamicSpawnLandmine_GameInit(self)
-    self.triggerRadius =
-        (self.TriggerType == "Proximity" or self.TriggerType == "Proximity-Timed") and
-            (1 + (CurrentModOptions.extra_proximityExplosive_radius or 0)) or 0
 
-    ObjModified(self)
-    Landmine.UpdateTriggerRadiusFx(self)
+    if self.class == "DynamicSpawnLandmine" then
+        self.triggerRadius = (self.TriggerType == "Proximity" or self.TriggerType ==
+                                 "Proximity-Timed") and
+                                 (1 + (CurrentModOptions.extra_proximityExplosive_radius or 0)) or 0
+        ObjModified(self)
+        self:UpdateTriggerRadiusFx()
+    end
 end
